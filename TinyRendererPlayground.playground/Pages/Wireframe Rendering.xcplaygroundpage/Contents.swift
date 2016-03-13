1f2478841b3efc41f6dde2e4da9adf2263850153
drhaynes <drhmbp@gmail.com>
2016-03-13 00:26:18 +0000

    Add image to wireframe rendering playground page

--- TinyRendererPlayground.playground/Pages/Wireframe Rendering.xcplaygroundpage/Contents.swift
+++ TinyRendererPlayground.playground/Pages/Wireframe Rendering.xcplaygroundpage/Contents.swift
@@ -1,7 +1,6 @@
 //: [Previous](@previous)
 
-import Foundation
-
-var str = "Hello, playground"
+let image = createImage(width: 320, height: 240)
+let uiImage = uiImageForImage(image)
 
 //: [Next](@next)
