56a607d7a1940d767c0c89cebeaa3104a9d3b412
drhaynes <drhmbp@gmail.com>
2016-03-13 01:51:50 +0000

    Parse obj file and create mesh

--- TinyRendererPlayground.playground/Pages/Wireframe Rendering.xcplaygroundpage/Contents.swift
+++ TinyRendererPlayground.playground/Pages/Wireframe Rendering.xcplaygroundpage/Contents.swift
@@ -3,7 +3,9 @@ import ModelIO
 
 let image = createImage(width: 320, height: 240)
 
-let modelPath = NSBundle.mainBundle().pathForResource("head", ofType: "obj")!
+let path = NSBundle.mainBundle().pathForResource("head", ofType: "obj")!
+
+let mesh = Mesh(objPath: path)
 
 let uiImage = uiImageForImage(image)
 //: [Next](@next)
@@ -28,24 +30,41 @@ public struct Vector3<T> {
 
 public struct Mesh {
     public let vertices: [Vector3<Float>]
-    public let faces: [Int]
+    public let faces: [Vector3<Int>]
 
-    public init(vertices: [Vector3<Float>], faces: [Int]) {
+    public init(vertices: [Vector3<Float>], faces: [Vector3<Int>]) {
         self.vertices = vertices
         self.faces = faces
     }
 
     public init?(objPath: String) {
-        guard let streamReader = StreamReader(path: modelPath) else {
+        guard let streamReader = StreamReader(path: objPath) else {
             return nil
         }
         defer {
             streamReader.close()
         }
+        var vertices = [Vector3<Float>]()
+        var faces = [Vector3<Int>]()
         while let line = streamReader.nextLine() {
             print(line)
+            let tokens = line.componentsSeparatedByString(" ")
+            switch tokens[0] {
+            case "v":
+                vertices.append(Vector3(Float(tokens[1])!, Float(tokens[2])!, Float(tokens[3])!))
+                break
+            case "f":
+                let triplet1 = tokens[1].componentsSeparatedByString("/")
+                let triplet2 = tokens[2].componentsSeparatedByString("/")
+                let triplet3 = tokens[3].componentsSeparatedByString("/")
+                let face = Vector3(Int(triplet1[0])! - 1, Int(triplet2[0])! - 1, Int(triplet3[0])! - 1)
+                faces.append(face)
+                break
+            default:
+                break
+            }
         }
-        self.vertices = [Vector3]()
-        self.faces = [Int]()
+        self.vertices = vertices
+        self.faces = faces
     }
 }
