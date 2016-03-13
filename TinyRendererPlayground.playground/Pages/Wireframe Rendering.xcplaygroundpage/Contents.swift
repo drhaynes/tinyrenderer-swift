c7018966a0186c2c687cb460bf7c9e878c5b22e2
drhaynes <drhmbp@gmail.com>
2016-03-13 01:26:43 +0000

    Add Vector3 and Mesh with stub init method

--- TinyRendererPlayground.playground/Pages/Wireframe Rendering.xcplaygroundpage/Contents.swift
+++ TinyRendererPlayground.playground/Pages/Wireframe Rendering.xcplaygroundpage/Contents.swift
@@ -5,14 +5,47 @@ let image = createImage(width: 320, height: 240)
 
 let modelPath = NSBundle.mainBundle().pathForResource("head", ofType: "obj")!
 
-if let aStreamReader = StreamReader(path: modelPath) {
-    defer {
-        aStreamReader.close()
+let uiImage = uiImageForImage(image)
+//: [Next](@next)
+
+public struct Vector3<T> {
+    let x: T
+    let y: T
+    let z: T
+
+    init(x: T, y: T, z: T) {
+        self.x = x
+        self.y = y
+        self.z = z
     }
-    while let line = aStreamReader.nextLine() {
-        print(line)
+
+    public init(_ x: T, _ y: T, _ z: T) {
+        self.x = x
+        self.y = y
+        self.z = z
     }
 }
 
-let uiImage = uiImageForImage(image)
-//: [Next](@next)
+public struct Mesh {
+    public let vertices: [Vector3<Float>]
+    public let faces: [Int]
+
+    public init(vertices: [Vector3<Float>], faces: [Int]) {
+        self.vertices = vertices
+        self.faces = faces
+    }
+
+    public init?(objPath: String) {
+        guard let streamReader = StreamReader(path: modelPath) else {
+            return nil
+        }
+        defer {
+            streamReader.close()
+        }
+        while let line = streamReader.nextLine() {
+            print(line)
+        }
+        self.vertices = [Vector3]()
+        self.faces = [Int]()
+    }
+}
