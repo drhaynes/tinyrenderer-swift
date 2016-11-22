9e52d7722f150844d6c5109fbc1b2c2f1f239cc5
drhaynes <drhmbp@gmail.com>
2016-11-22 10:28:46 +0000

    Added todo.txt

--- TinyRendererPlayground.playground/Pages/Flat Shading.xcplaygroundpage/Contents.swift
+++ TinyRendererPlayground.playground/Pages/Flat Shading.xcplaygroundpage/Contents.swift
@@ -5,7 +5,7 @@ import Foundation
 let image = Image(width: 640, height: 640)
 let path = NSBundle.mainBundle().pathForResource("head", ofType: "obj")!
 let mesh = Mesh(objPath: path)!
-let lightDirection = normalise(Vector3<Float>(-0.5, -0.5, -1))
+let lightDirection = normalise(Vector3<Float>(0, 0, -1))
 
 renderFlatShaded(mesh, image: image, lightDirection: lightDirection)
 
